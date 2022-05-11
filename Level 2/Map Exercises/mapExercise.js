9819cd7dd3167c9bee41ced07ba2732d96c80dfa
madeline svoren <madelinesvoren@gmail.com>
2022-05-10 20:59:04 -0400

    Numbers to string using .map

--- Level 2/Map Exercises/mapExercise.js
+++ Level 2/Map Exercises/mapExercise.js
@@ -10,9 +10,25 @@ console.log(result)
 
 //Making array of numbers into a string
 
-const result = arr.map(function stringItUp(arr){
-    return result
+const result2 = result.map(function stringItUp(item){
+    return item.toString();
+    
+
   })
   
-  console.log(result)
+  console.log(result2)
+  
+//Cap first letter lowercase the rest
+
+const namesArr = ["john", "JACOB", "jinGleHeimer", "schmidt"]
+
+namesArr = namesArr.map((str, [1]) => ({john: str, id: [1] + 1}))
+
+const capNames = splitName1.map(function capital(name){
+    return splitName1.toUpperCase
+
+})
+
+  console.log(capNames); 
+  
   
